21a02322b962746286d1e15a17c3bfdb3bcf7f39
Lidia J. Mayta Huanca <224844611+mhlidia@users.noreply.github.com>
2026-03-18 13:52:26 -0400

    BTN-8: Implement a global middleware that validates the JWT token on each protected route (#8)
     was applied to the protected routes

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const asyncHandler = require("../utils/asyncHandler");
+const authMiddleware = require("../middlewares/auth.middleware");
+
 const categoryController = require("../controllers/category.controller");
 
-router.get("/", asyncHandler(categoryController.index));
-router.post("/", asyncHandler(categoryController.store));
-router.put("/:id", asyncHandler(categoryController.update));
-router.delete("/:id", asyncHandler(categoryController.destroy));
+router.get("/", authMiddleware, asyncHandler(categoryController.index));
+router.post("/", authMiddleware, asyncHandler(categoryController.store));
+router.put("/:id", authMiddleware, asyncHandler(categoryController.update));
+router.delete("/:id", authMiddleware, asyncHandler(categoryController.destroy));
 
 module.exports = router;
